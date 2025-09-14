b1c80fc65a12a2bf3d39880ed7dd031fb531d45f
Tohir <itsmetohir@gmail.com>
2025-09-14 09:31:25 +0500

    bug fix

--- vars/DeployApp.groovy
+++ vars/DeployApp.groovy
@@ -3,26 +3,25 @@ def call(Map config = [:]) {
     def appName  = config.appName ?: "app"
     def portMap  = config.portMap ?: "3000:3000"
 
-    stages {
-        stage("Deploy") {
-            steps {
-                script {
-                    sh """
-                    echo "Pulling image: ${image}"
-                    docker pull ${image}
+    stage("Deploy") {
+        steps {
+            script {
+                sh """
+                echo "Pulling image: ${image}"
+                docker pull ${image}
 
-                    echo "Stopping old containers for ${appName}..."
-                    docker ps -q --filter "ancestor=${image.split(':')[0]}" | xargs -r docker stop
-                    docker ps -a -q --filter "ancestor=${image.split(':')[0]}" | xargs -r docker rm
+                echo "Stopping old containers for ${appName}..."
+                docker ps -q --filter "ancestor=${image.split(':')[0]}" | xargs -r docker stop
+                docker ps -a -q --filter "ancestor=${image.split(':')[0]}" | xargs -r docker rm
 
-                    echo "Removing old container with same name (if any)..."
-                    docker rm -f ${appName} || true
+                echo "Removing old container with same name (if any)..."
+                docker rm -f ${appName} || true
 
-                    echo "Running new container..."
-                    docker run -d --name ${appName} -p ${portMap} ${image}
-                    """
-                }
+                echo "Running new container..."
+                docker run -d --name ${appName} -p ${portMap} ${image}
+                """
             }
         }
-    } 
-}
+    }
+} 
+
